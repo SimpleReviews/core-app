982bd297b36841a505b5850b26c5ea65075a72e6
Chad Miller <chadtmiller15@gmail.com>
2014-08-05 21:02:17 -0700

    added categories collection fixtures to index view

--- public/js/views/categories/index.js
+++ public/js/views/categories/index.js
@@ -1,12 +1,22 @@
 var $ = require('jquery');
 var Backbone = require('backbone');
 Backbone.$ = $;
-
+var Category = require('../../models/category');
+var Categories = require('../../collections/categories');
 var template = require('../../../templates/categories/index.hbs');
 
 var CategoriesIndexView = Backbone.View.extend({
+  collection: new Categories(),
+  initialize: function() {
+    this.collection.add(Category.FIXTURES);
+    this.listenTo(this.collection, 'all', this.render);
+  },
   render: function () {
-    this.$el.html(template);
+    var data = [];
+    this.collection.models.forEach(function(item) {
+      data.push({name: item.get('name')});
+    });
+    this.$el.html(template({categories: data}));
   }
 });
 
