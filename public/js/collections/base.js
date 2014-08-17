fc948b6a9708ddbff90baa014bad976b0e7e0cae
Chad Miller <chadtmiller15@gmail.com>
2014-08-16 21:24:18 -0700

    getOrFetch method now returns a promise

--- public/js/collections/base.js
+++ public/js/collections/base.js
@@ -1,12 +1,21 @@
 var Backbone = require('backbone');
+var kew = require('kew');
 
 module.exports = Backbone.Collection.extend({
   getOrFetch: function(id) {
     var model = this.get(id);
+    var deferred = kew.defer();
+
     if (!model) {
       model = this.add({id: id});
-      model.fetch();
+      model.fetch()
+        .then(function() {
+          deferred.resolve(model);
+        });
+    } else {
+      deferred.resolve(model);
     }
-    return model;
+
+    return deferred.promise;
   }
 });
