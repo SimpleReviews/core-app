978a9b18d9110b3847183d3564f1600941a93572
Abraham Antigua <abrahamantigua@gmail.com>
2014-08-20 01:57:51 -0700

    theyre coming to get you barbara

--- public/js/views/app.js
+++ public/js/views/app.js
@@ -9,36 +9,31 @@ module.exports = View.extend({
   template: template,
 
   initialize: function(options) {
-    console.log('app view');
-
-    Auth.fetch()
-      .fail(function() {
-        console.error('Unauthenticated');
-      });
-
-    this.spinner = new SpinView({ size: 'large' });
-    this.render();
-    this.renderNavView();
-  },
+      console.log('app view');
+  
+      Auth.fetch()
+        .fail(function() {
+                console.error('Unauthenticated');
+              });
+  
+      this.spinner = new SpinView({ size: 'large' });
+      this.render();
+      this.renderNavView();
+    },
 
   showSpinner: function() {
-    this.spinner.setElement(this.$('#detail'));
-    this.spinner.render();
-  },
+      this.spinner.setElement(this.$('#detail'));
+      this.spinner.render();
+    },
 
   renderNavView: function() {
-    this.navView = new NavView();
-    this.navView.render();
-    this.$('#nav').html(this.navView.el);
-  },
+      this.navView = new NavView();
+      this.navView.setElement(this.$('#nav'));
+      this.navView.render();
+    },
 
   renderChildView: function(view) {
-    if (this.currentView){
-      this.currentView.remove();
+      view.setElement(this.$('#detail'));
+      view.render();
     }
-
-    this.currentView = view;
-    this.currentView.render();
-    this.$('#detail').html(this.currentView.el);
-  }
 });
